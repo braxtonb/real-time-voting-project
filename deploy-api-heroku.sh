6c8efe9b255f0b0d9015a4a2bfab9d39c2db9292
Braxton Brewton <braxtonbrewton@gmail.com>
2020-09-08 17:57:01 -0400

    added links providing information regarding heroku subtree pushes p2

---
+++ deploy-api-heroku.sh
@@ -0,0 +1,3 @@
+# https://stackoverflow.com/a/10648623
+# https://github.com/heroku/heroku-buildpack-nodejs/issues/385#issuecomment-378836793
+git subtree push --prefix real-time-voting-api heroku master
